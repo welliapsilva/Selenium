81372933c3b5407acd6ad7d4ac600c80c576524c
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-17 22:48:22 -0300

    codgo base

--- codigoBase.py
+++ codigoBase.py
@@ -5,21 +5,38 @@ from selenium.webdriver.chrome.options import Options
 #a função abaixo esta minimizada clicando na ceta a esquerda ela abre
 def iniciar_driver():
     chrome_options = Options()
-    arguments = ['--lang=pt-BR', '--window-size=800,600', '--incognito']
+    # Fonte de opções de switches https://peter.sh/experiments/chromium-command-line-switches/
+
+    arguments = ['--lang=pt-BR', '--window-size=800,600','--incognito']
+    ''' Common arguments
+    --start-maximized # Inicia maximizado
+    --lang=pt-BR # Define o idioma de inicialização, # en-us , pt-BR
+    --incognito # Usar o modo anônimo
+    --window-size=800,800 # Define a resolução da janela em largura e altura
+    --headless # Roda em segundo plano(com a janela fechada)
+    --disable-notifications # Desabilita notificações
+    --disable-gpu # Desabilita renderização com GPU
+    '''
     for argument in arguments:
         chrome_options.add_argument(argument)
 
-    chrome_options.add_experimental_option('prefs', {
+    caminho_padrao_para_download = 'C:\\Users\\WellingtonAS\\Downloads'
+
+    # Lista de opções experimentais(nem todas estão documentadas) https://chromium.googlesource.com/chromium/src/+/32352ad08ee673a4d43e8593ce988b224f6482d3/chrome/common/pref_names.cc
+    chrome_options.add_experimental_option("prefs", {
+        'download.default_directory': caminho_padrao_para_download,
+        # Atualiza diretório para diretório passado acima
+        'download.directory_upgrade': True,
+        # Setar se o navegar deve pedir ou não para fazer download
         'download.prompt_for_download': False,
-        'profile.default_content_setting_values.notifications': 2,
-        'profile.default_content_setting_values.automatic_downloads': 1,
-
+        "profile.default_content_setting_values.notifications": 2,  # Desabilita notificações
+        # Permite realizar múltiplos downlaods multiple downloads
+        "profile.default_content_setting_values.automatic_downloads": 1,
     })
-    driver = webdriver.Chrome(options=chrome_options)
 
+    driver = webdriver.Chrome(options=chrome_options)
     return driver
 
-
 driver = iniciar_driver()
-driver.get('https://facebook.com')
-input('digite algo para fechar... ')
+driver.get('https://www.devaprender.com')
+input('')
