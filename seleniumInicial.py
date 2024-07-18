e6fb0595d2282cff550a6d0cdd2c0810c096bf97
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-17 22:32:35 -0300

    nova versão atual

--- seleniumInicial.py
+++ seleniumInicial.py
@@ -1,10 +1,8 @@
 #inicial para usar o selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 # inicializando o webdriver
-driver= webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))#instalando as dependencias do google Chrome do computador
+driver= webdriver.Chrome()#instalando as dependencias do google Chrome do computador
 
 #navegar ate um site usando driver
 driver.get('https://www.facebook.com')
