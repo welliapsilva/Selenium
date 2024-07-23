8fc5ceed5a3d697c427151ac8c29c44be5ed8939
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-22 21:51:10 -0300

    cclicar em checkbox

---
+++ clicarCheckBox.py
@@ -0,0 +1,38 @@
+from tabnanny import check
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,1000', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(options=chrome_options)
+
+    return driver
+
+
+driver = iniciar_driver()
+# navegar até o site
+driver.get('https://cursoautomacao.netlify.app/desafios.html')
+
+driver.execute_script('window.scrollTo(0,500)')
+sleep(1)
+
+checkbox_conversivel = driver.find_element(By.ID, 'conversivelcheckbox')
+checkbox_offroad = driver.find_element(By.ID, 'offroadcheckbox')
+
+checkbox_conversivel.click()
+checkbox_offroad.click()
+input('')
+driver.close()
