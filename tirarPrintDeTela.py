b4aeb4e4f7f32a59e3fb0df3c5ab6db86e5a39b9
Wellington Aparecido Silva <welliapsilva@gmail.com>
2024-07-27 00:12:45 -0300

    print de tela

---
+++ tirarPrintDeTela.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments = ['--lang=pt-BR', '--window-size=800,1000', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    driver = webdriver.Chrome(options=chrome_options)
+
+    return driver
+
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app')
+driver.maximize_window()
+driver.save_screenshot('tela.jpg')
+input('')
+driver.close()
